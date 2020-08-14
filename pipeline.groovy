606e82d9f90d0a315369662854cd29cd0e32971f
user lap <upannala@gmail.com>
2020-08-14 20:20:45 +0530

    test

---
+++ pipeline.groovy
@@ -0,0 +1,15 @@
+pipeline{
+    agent{
+        label 'master'
+    }
+    
+    stages{
+        stage{
+            steps{
+                script{
+                    
+                }
+            }
+        }
+    }
+}
